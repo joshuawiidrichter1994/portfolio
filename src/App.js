6828f975d83c9cdb901cfb0184a58cf72e06e854
Joshua Richter <joshuawiidrichter@gmail.com>
2024-02-18 16:19:50 +0200

    Added structure / styling to portfolio containers

--- src/App.js
+++ src/App.js
@@ -10,17 +10,56 @@ function App() {
         <div className="right-block">
           <div className="text-container">
             <a href="https://www.linkedin.com/in/joshua-richter-5373329b/">
-              <p className="text-black">Cape Town based web developer</p>
+              <p className="text-header">Cape Town based web developer</p>
             </a>
           </div>
         </div>
       </header>
       <main>
+        {/* ROW 1 */}
         <div className="row">
-          <div className="portfolio-container"></div>
-          <div className="portfolio-container"></div>
-          <div className="portfolio-container"></div>
+          <div className="portfolio-container">
+            <p className="text-black">Purrfection</p>
+            <div className="photo-container">
+              <img
+                className="portfolio-photo"
+                src={require('./images/purrfection.jpg')}
+              />
+            </div>
+            <p className="portfolio-description">
+              Purrfection is a Cape Town based artist. He as only released one
+              single, but there's more to come
+            </p>
+          </div>
+          <div className="portfolio-container">
+            <p className="text-black">Music Production</p>
+            <div className="photo-container">
+              <img
+                className="portfolio-photo"
+                src={require('./images/music-production.jpg')}
+              />
+            </div>
+            <p className="portfolio-description">
+              My years of collective experience dedicated to mixing, mastering,
+              and music production
+            </p>
+          </div>
+          <div className="portfolio-container">
+            <p className="text-black">Music Production</p>
+            <div className="photo-container">
+              <img
+                className="portfolio-photo"
+                src={require('./images/gaby-photography.jpg')}
+              />
+            </div>
+            <p className="portfolio-description">
+              Through the absence of color, the focus shifts to the intricate
+              textures, graceful curves, and subtle contrasts of light and
+              shadow, highlighting the innate beauty of each bloom.
+            </p>
+          </div>
         </div>
+        {/* ROW 2 */}
         <div className="row">
           <div className="portfolio-container"></div>
           <div className="portfolio-container"></div>

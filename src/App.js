78f1c52e08c80157254cdd880902001e515f2179
Joshua Richter <joshuawiidrichter@gmail.com>
2024-02-18 14:32:15 +0200

    Added header & styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Joshua portfolio</h1>
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;

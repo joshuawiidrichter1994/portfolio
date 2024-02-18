6cea260efd853039134b357307f886457afeaa8e
Joshua Richter <joshuawiidrichter@gmail.com>
2024-02-18 14:50:23 +0200

    Modified heder styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Joshua portfolio</h1>
+        <div className="left-block">
+          <h1>JR</h1>
+        </div>
+        <div className="right-block">
+          <p className="header-p">ABOUT</p>
+          <p className="header-p">CONTACT</p>
+        </div>
       </header>
       <main></main>
     </div>

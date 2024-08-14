2677afa37e06e21610bcd6d5ae71a8985f63b9be
Joshua Richter <joshuawiidrichter@gmail.com>
2024-08-14 12:38:48 +0200

    Modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,21 @@ function Footer() {
     <div>
       <footer>
         <div className="footer-container">
-          <p className="text-white">
-            web developer, mixing / mastering engineer, and producer
-          </p>
+          <div className="footer-contact">
+            <p className="normal-text-white">
+              If you are interested in my services, please submit your email
+              below and I will get back to you ASAP
+            </p>
+            <form>
+              <label>Email</label>
+              <input type="text" placeholder="Email"></input>
+              <button type="submit">Submit</button>
+            </form>
+            <p className="normal-text-white">
+              Alternatively, drop me a mail at joshuawiidrichter@gmail.com
+            </p>
+          </div>
+          <div className="footer-socials"></div>
         </div>
       </footer>
     </div>

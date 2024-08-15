b12db91f4d44538ca6075dddb094243ac1af9b46
Joshua Richter <joshuawiidrichter@gmail.com>
2024-08-15 15:49:04 +0200

    Updated footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,15 @@ function Footer() {
               If you are interested in my services, please submit your email
               below and I will get back to you ASAP
             </p>
-            <form>
-              <label>Email</label>
-              <input type="text" placeholder="Email"></input>
-              <button type="submit">Submit</button>
+            <form className="contact-form">
+              <input
+                type="text"
+                placeholder="Email"
+                className="email-field"
+              ></input>
+              <button type="submit" className="submit-btn">
+                Submit
+              </button>
             </form>
             <p className="normal-text-white">
               Alternatively, drop me a mail at joshuawiidrichter@gmail.com
